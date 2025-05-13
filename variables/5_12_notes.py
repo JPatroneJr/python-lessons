8e7e66b0269e6ab1564e67463b9adb5124447df0
JosephPatroneJr <101902051+JosephPatroneJr@users.noreply.github.com>
2025-05-12 21:44:27 -0400

    5_12 notes

---
+++ variables/5_12_notes.py
@@ -0,0 +1,78 @@
+# py test.py 
+
+
+'''
+Comments-
+The quotes surrounding this denote a multi-line comment in this case.
+#A single line comment will have a pound sign in front of it like this one.
+
+Any commented block/line WILL NOT be executed. so you can type anything here.
+Mostly notes, keep it clean folks.
+
+---------------------------------------------------------------------------
+Variables-
+Variables are just like you remember in algebra, they can be set to anything 
+and will change the formula/operation they are used in 
+
+x = 1
+1 + x = 2
+
+x = 4
+1 + x = 5
+
+---------------------------------------------------------------------------
+Variable Types-
+For now we will stick with the basics.
+String (str)
+Integer (int)
+Float(float)
+Boolean Value (bool)  --- True or False
+'''
+
+
+var1 = 'x'
+var2 = 1
+var3 = '1'
+var4 = 1.1
+var5 = True
+
+# this is the 'operation' with its variable
+# change the variables out of the ones above or make new ones and see what comes out
+# if you get an error dont worry!
+
+print(f'The variable is {var1}')
+print(f'The variables type is {type(var1)}')
+
+
+print(f'The variables type is {type(var1)}')
+# functions
+
+
+# print() -> returns text to the screen
+# type(variable) -> returns the python type of the passed variable
+# f'{var1}'
+
+
+# <name_of_function>()
+
+
+
+
+
+# strings 
+# x  = 'im a single quote 
+# string'
+
+# x  = '''im a single quote 
+# string'''
+
+# x  = """im a single quote 
+# string"""
+
+
+# "im a double quote string"
+
+# long string
+# '''
+
+# '''
